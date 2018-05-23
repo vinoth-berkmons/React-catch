bf965603fb87eb666b075a0c8574815eed8a9d2b
Vinoth <vinoth.b@eternushealth.com>
2018-05-23 20:52:10 +0530

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+ 
+class App extends React.Component {
+	render() {
+		return (
+			<div className = "catch-of-the-day">
+				<p> Hey hooo! </p>
+			</div>
+			)
+	}
+}
+
+export default App;
